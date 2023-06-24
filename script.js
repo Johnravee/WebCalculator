3f325125c459e4c4e23367b782bea8e46daa15dc
Johnravee <johnravemimayn@gmail.com>
2023-06-24 15:59:06 +0800

    Functions

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 const dis = document.querySelector(".display");
 const btn = document.querySelector(".btn")
 
- btn.addEventListener('click',(e)=>{
-     
-    //*Display a value of click btn
+ btn.addEventListener('click', Display)
+ document.querySelector(".equal").addEventListener("click", Equal)
+ document.querySelector(".back").addEventListener("click", Ce)
+
+ function Display(e){
+      //*Display a value of click btn
     let btnClick = e.target.value
     
     //*Pass the value of any button that clicked
     dis.append(btnClick)
-    
- })
+ }
 
- document.querySelector(".equal").addEventListener("click",()=>{ 
-   //*Tested for posibble errors
-   try{
-     //*evaluate the number input in display
-     let result = eval(dis.innerHTML)
+ function Equal(){
+    //*Tested for posibble errors
+    try{
+      //*evaluate the number input in display
+      let result = eval(dis.innerHTML)
+ 
+       //*display result 
+      dis.innerHTML = result
+ 
+      //*No display
+     if(dis.innerHTML === "undefined") dis.innerHTML = ""
+ 
+     //*Any Error Produce will pass to err
+    }catch(err){
+     alert("Error")
+    } 
+ }
 
-      //*display result 
-     dis.innerHTML = result
-
-     //*No display
-    if(dis.innerHTML === "undefined") dis.innerHTML = ""
-
-    //*Any Error Produce will pass to err
-   }catch(err){
-    alert("Error")
-   } 
- })
-
- document.querySelector(".back").addEventListener("click", ()=>{
+ function Ce(){
     //*store the value of display to a variable
-   let str = dis.innerHTML
+    let str = dis.innerHTML
 
-   //*display updated value na nabawasan na.
-   dis.innerHTML = str.substring(0,str.length - 1)
+    //*display updated value na nabawasan na.
+    dis.innerHTML = str.substring(0,str.length - 1)
+ }
 
-})
 
 //* Delete all display
  document.querySelector(".del").addEventListener("click",()=>{
